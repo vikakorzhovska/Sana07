468c7b38cf79223c5eb037dcb45374e7eb7a22fc
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2024-01-20 13:29:50 +0200

    Created class ShoppingCart

---
+++ Sana07/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana07
+{
+    public class ShoppingCart
+    {
+        private List<Product> items;
+        public ShoppingCart() 
+        { 
+            items = new List<Product>();
+        }
+        public List<string> ItemsInfo()
+        {
+            List<string> itemStrings = new List<string>();
+            foreach (var item in items)
+            {
+                itemStrings.Add(item.Display());
+            }
+            return itemStrings; 
+        }
+        public decimal TotalPrice()
+        {
+            decimal sum = 0;
+            foreach (var item in items)
+            {
+                sum += item.Price * item.Count;
+            }
+            return sum;
+        }
+    }
+}
